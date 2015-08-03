c7d0866d2b65671720016040f1e056627a3c99e0
Zachary Hanif <zh4990@Zacharys-MacBook-Pro-2.local>
2015-08-03 07:00:23 -0700

    LICENCE and minor changes for types

--- src/main/scala/org/novetta/zoo/types/CommandTypes.scala
+++ src/main/scala/org/novetta/zoo/types/CommandTypes.scala
@@ -1,8 +1,39 @@
 package org.novetta.zoo.types
 
+/**
+ * Create() case class. Used between the ConsumerActor and the WorkGroup.
+ * @param key: Long => The message key associated with this work.
+ * @param primaryURI: String => The primary URL for downloading the target resource
+ * @param secondaryURI: String => The secondary URL for downloading the target resource
+ * @param value: WorkState => The state of the Job, and component work at time of creation
+ *
+ * @constructor Generate a Create message. This is used to initiate the creation of a WorkActor
+ *
+ */
 case class Create(key: Long, primaryURI: String, secondaryURI: String, value: WorkState)
 
+/**
+ * Result case class. Used between the WorkActor and the ProducerActor
+ * @param filename: String => The filename representing the target of this Job
+ * @param result: WorkResult => The WorkResult representing the end state of the Job
+ *
+ * @constructor Generate a Result message. This is used to transmit results to the Producer and from there, to the queueing backbone
+ *
+ */
 case class Result(filename: String, result: WorkResult)
+
+/**
+ * ResultPackage case class. A package of results for transmission. The various filehashes are used for secondary aggregation against
+ * the target processing file, that information is lost due to the UUID usage in the temporary filestore.
+ * @param filename: String => The filename representing the target of this Job
+ * @param results: Iterable[WorkResult] => An Iterable representing the WorkResults to be transmitted
+ * @param MD5: String => MD5 hash of the target file.
+ * @param SHA1: String => SHA1 hash of the target file.
+ * @param SHA256: String => SHA256 hash of the target file.
+ *
+ * @constructor Generate a ResultPackage message. This is for multiple WorkResult transfers.
+ *
+ */
 case class ResultPackage(filename: String, results: Iterable[WorkResult], MD5: String, SHA1: String, SHA256: String)
 
 object WorkState {
@@ -11,6 +42,19 @@ object WorkState {
   }
 }
 
+/**
+ * WorkState case class. A representation of the current state of a given Job. This can be used to merge Jobs, or transfer overall Job state.
+ * @param filename: String => The filename representing the target of this Job.
+ * @param hashfilename: String => The hashed filename representing the target of the Job.
+ * @param workToDo: List[TaskedWork] => A list of all TaskedWork elements, which are the component Work elements
+ * @param created: Int => The time this work was created.
+ * @param lastEdited: Int => The last time this work had an altered state.
+ * @param results: List[WorkResult] => A list of the WorkResults that have been generated.
+ * @param attempts: Int => The number of times this Job has been attempted across all executors.
+
+ * @constructor Generate a WorkState message.
+ *
+ */
 case class WorkState(
                       filename: String,
                       hashfilename: String,

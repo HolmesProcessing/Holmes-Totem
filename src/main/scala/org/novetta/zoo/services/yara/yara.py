194df8e5c79e29e362b9fa7547636cbbdd63d99a
webstergd <webstergd@sec.in.tum.de>
2015-12-03 15:10:05 +0100

    Service now allows you to use a default local or remote YARA rule set. The rules are pulled and compiled on the Docker container build.
    The service worker also has the base code needed to allow for custom rules to be sent during tasking.
    
    Additionally, this change switch yara.compile to yara.load to provide some performance boost. This was done becasue we are compiling the rules on the Docker build and only allowing compiles custom rules.

--- src/main/scala/org/novetta/zoo/services/yara/yara.py
+++ src/main/scala/org/novetta/zoo/services/yara/yara.py
@@ -1,6 +1,6 @@
 # imports for tornado
 import tornado
-from tornado import web, httpserver
+from tornado import web, httpserver, iploop
 
 # imports for logging
 import traceback
@@ -8,10 +8,11 @@ import os
 from os import path
 
 # imports for yara to work
-import binascii
-import yara
 import akka
+import binascii
+import ConfigParser
 import sys
+import yara
 
 
 class YaraHandler(tornado.web.RequestHandler):
@@ -19,62 +20,73 @@ class YaraHandler(tornado.web.RequestHandler):
 	def YaraEngine(self):
 		if not self.application.engine:
 			try:
-				self.application.engine = yara.compile(sys.argv[1])
+				self.application.engine = yara.load(sys.argv[1])
 			except Exception as e:
 				print e
 		return self.application.engine
 
 
 class YaraProcess(YaraHandler):
-	def process(self, tup):
+	def process(self, tup, rule=None):
 		try:
-			results = self.YaraEngine.match(tup)
+			if rules:
+				rules = yara.load(rules)
+				results = rules.match(tup)
+			else:
+				results = self.YaraEngine.match(tup)
 			results2 = map(lambda x: {"rule": x.rule}, results)
 			return results2
 		except Exception, e:
 			return e
 
-    def get(self, filename):
-        try:
-            fullPath = os.path.join('/tmp/', filename)
-            data = self.process(fullPath)
-            self.write({"yara": data)
-        except Exception as e:
-            self.write({"error": traceback.format_exc(e)})
+	def get(self, filename):
+		try:
+			fullPath = os.path.join('/tmp/', filename)
+			data = self.process(fullPath)
+			self.write({"yara": data)
+		except Exception as e:
+			self.write({"error": traceback.format_exc(e)})
+
+	def post(self, filename):
+		try:
+			fullPath = os.path.join('/tmp/', filename)
+			custom_rule = self.files['custom_rule'][0]
+			data = self.process(fullPath, custom_rule)
+			self.write({"yara": data)
+		except Exception as e:
+			self.write({"error": traceback.format_exc(e)})
 
 
 class Info(tornado.web.RequestHandler):
-    # Emits a string which describes the purpose of the analytics
-    def get(self):
-        description = """
+	# Emits a string which describes the purpose of the analytics
+	def get(self):
+		description = """
 Provides Yara signature matching for samples using a collective 
 set of signatures or a provided custom signature.
 Copyright 2015 Holmes Processing
-        """
-        self.write(description)
+		"""
+		self.write(description)
 
 
 class YaraApp(tornado.web.Application):
-    def __init__(self):
-        handlers = [
-            (r'/', Info),
-            (r'/yara/([a-zA-Z0-9\-]*)', YaraProcess),
-        ]
-        settings = dict(
-            template_path=path.join(path.dirname(__file__), 'templates'),
-            static_path=path.join(path.dirname(__file__), 'static'),
-            # xsrf_cookies=True,
-            # cookie_secret='dsfretghj867544wgryjuyki9p9lou67543/Vo=',
-        )
-        tornado.web.Application.__init__(self, handlers, **settings)
-        self.engine = None
+	def __init__(self):
+		handlers = [
+			(r'/', Info),
+			(r'/yara/([a-zA-Z0-9\-]*)', YaraProcess),
+		]
+		settings = dict(
+			template_path=path.join(path.dirname(__file__), 'templates'),
+			static_path=path.join(path.dirname(__file__), 'static')
+		)
+		tornado.web.Application.__init__(self, handlers, **settings)
+		self.engine = None
 
 
 def main():
-    server = tornado.httpserver.HTTPServer(YaraApp())
-    server.listen(7701, address="127.0.0.1")
-    tornado.ioloop.IOLoop.instance().start()
+	server = tornado.httpserver.HTTPServer(YaraApp())
+	server.listen(7701, address="127.0.0.1")
+	tornado.ioloop.IOLoop.instance().start()
 
 
 if __name__ == '__main__':
-    main()
+	main()

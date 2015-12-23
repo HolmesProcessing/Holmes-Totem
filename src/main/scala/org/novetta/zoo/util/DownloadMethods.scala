4d353289ce103ca581f3003c652684426b22e497
webstergd <webstergd@sec.in.tum.de>
2015-12-23 21:45:15 +0100

    removed legacy work

--- src/main/scala/org/novetta/zoo/util/DownloadMethods.scala
+++ src/main/scala/org/novetta/zoo/util/DownloadMethods.scala
@@ -7,13 +7,8 @@ import org.slf4j.LoggerFactory
 
 case class DownloadSettings(download_directory: String, request_timeout: Int, connect_timeout: Int)
 
-
 object DownloadMethods extends Instrumented with LazyLogging {
   val log = Logger(LoggerFactory.getLogger("name"))
-
-  val download_directory:
-  val requestTimeout:
-  val connectionTimeout:
 
   def MD5(s: Array[Byte]): String = {
     MessageDigest.getInstance("MD5").digest(s).map("%02X".format(_)).mkString

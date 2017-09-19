c11297d1b86140daa878b1778d923db70aa93422
Gani Qinami <gani_qinami@hotmail.com>
2017-09-19 16:43:13 +0200

    Add error handling and max size for PE binaries

--- src/main/scala/org/holmesprocessing/totem/services/cfgangr/convertbinary.py
+++ src/main/scala/org/holmesprocessing/totem/services/cfgangr/convertbinary.py
@@ -1,21 +1,37 @@
 import angr
-
+import simuvex
+import cle
+import os
+import json
 # imports for graph handling
 import networkx as nx
 from networkx.readwrite import json_graph
 
 
-def generateCFG(binary, analysisType = 'Fast'):
-    # Create the angr project for the binary
-    project = angr.Project(binary, auto_load_libs = False)
+def generateCFG(binary, max_size, analysisType = 'Fast'):
+    response = {}
+    try:
+        # Create the angr project for the binary
+        project = angr.Project(binary, auto_load_libs=False)
+        binary_size = os.stat(binary).st_size
+        max_binary_size = int(max_size) * 1024  # Here we will set a limit of binary files in bytes
+        if binary_size > max_binary_size and type(project.loader.main_bin).__name__ == 'PE':
+            response['error'] = 'CFG generation failed because the PE binary size is too big'
+            return json.dumps(response)
+        # Create the Control Flow Graph in Fast or Accurate mode.
+        if (analysisType == 'Fast'):
+            cfg = project.analyses.CFG()
+        elif (analysisType == 'Accurate'):#
+            cfg = project.analyses.CFGAccurate()
+        else:
+            return
 
-    # Create the Control Flow Graph in Fast or Accurate mode.
-    if (analysisType == 'Fast'):
-        cfg = project.analyses.CFG()
-    elif (analysisType == 'Accurate'):#
-        cfg = project.analyses.CFGAccurate()
-    else:
-        return
+    except simuvex.s_errors.SimSolverModeError:
+        response['error'] = 'CFG generation failed because of SimSolverModeError'
+        return json.dumps(response)
+    except cle.errors.CLECompatibilityError:
+        response['error'] = 'CFG generation failed because of unsupported format'
+        return json.dumps(response)
 
     # Iterate through all nodes, and for each of them, add information for address, mnemonic and operands.
     for node in cfg.nodes_iter():
